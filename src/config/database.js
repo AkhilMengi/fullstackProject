c7b76926c80821fd013b9f253b0d95175f651181
akhil <akhilmengi7@gmail.com>
2025-07-17 15:59:25 +0530

    cluster connection is created

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose")
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/devTinder")
+    await mongoose.connect("mongodb+srv://myuser:Akhil19971324@cluster0.3rslsce.mongodb.net/")
 }  
 
 
