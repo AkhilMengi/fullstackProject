e2bbbd0bd13e85a22ee52f35cce76494e3af509a
akhil <akhilmengi7@gmail.com>
2025-07-26 23:38:20 +0530

    /api is added in app.js file

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ app.use(cors({
     credentials:true
 }))
 
-app.use('/',authRouter)
-app.use('/',profileRouter)
-app.use('/',requestRouter)
-app.use('/',userRouter)
+app.use('/api', authRouter);
+app.use('/api', profileRouter);
+app.use('/api', requestRouter);
+app.use('/api', userRouter);
 
 connectDB()
     .then(() => {
